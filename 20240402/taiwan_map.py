66d92a8c8a6e8dfed3743af6ddfdd0142594633a
10922321 <s10922321@cycu.edu.tw>
2024-04-02 19:14:41 +0800

    02

--- 20240402/taiwan_map.py
+++ 20240402/taiwan_map.py
@@ -6,14 +6,18 @@ import geopandas as gpd
 import pandas as pd
 
 #read shape file
-taiwan=gpd.read_file('20240402/county/county_moi_1090820.shp')
+taiwan=gpd.read_file('C:/Users/User/Documents/GitHub/cycu_ai2024/20240402/county/county_moi_1090820.shp')
 import os
 #print cwd
-print (taiwan.shape)
+print (taiwan)
 
 #plot taiwan using matplotlib
 import matplotlib.pyplot as plt
+
+#輸出台灣地圖的時候 圖面顯示的範圍 緯度最低為21.5度 緯度最高為25.5度 經度最低為119度 經度最高為122度
 taiwan.plot()
+plt.xlim(119,122)
+plt.ylim(21.5,25.5)
 plt.show()
 #save to png file
 plt.savefig('20240402/taiwan_map.png')
